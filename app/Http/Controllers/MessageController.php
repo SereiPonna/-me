da5962d485b9ba7d9d4c4b06f8d69a04b7de9bc4
SereiPonna <sereiponna@SereiPonna.local>
2017-12-03 14:35:31 +0700

    final test

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -8,15 +8,11 @@ use GuzzleHttp\Client;
 class MessageController extends Controller
 {
     public function send(Request $request) {
-
-        $message = $request->input('messageText');
-
+        $message = $request->input('body');
         $destination = ["tel:AZ1104k9foY3ZCpCcyRVzUXopzt1DA7KF6NfvCscTfqZe+ACqGgm3dkf/Xn1yEHdzDJwJ"];
-
         $client = new Client([
             'headers' => [ 'Content-Type' => 'application/json' ]
         ]);
-
         $response = $client->post('https://api.dialog.lk/sms/send',
             ['body' => json_encode(
                 ["applicationId" => "APP_041235",
@@ -26,6 +22,6 @@ class MessageController extends Controller
                 ]
             )]
         );
-
+        error_log($message);
     }
 }
